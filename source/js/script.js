44171b1181764c3d8eadfa9a43b8aa8f889ceba4
WearponiX <timofey_volk@mail.ru>
2019-05-24 18:16:23 +0300

    Б30 и Д14

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ window.onload = function() {
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
   navMain.classList.remove('main-nav--nojs');
+  navMain.classList.remove('main-nav--opened');
+  navMain.classList.add('main-nav--closed');
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
